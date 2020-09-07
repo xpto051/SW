ba0e7095e2a05a9ea40be6ae74daf74d0497af07
Netrod <netrod@live.com.pt>
2020-09-07 05:06:17 +0100

    Student List
    Mostly done, falta corrigir details

--- GEP/ClientApp/src/app/main/students-list/students-list.component.ts
+++ GEP/ClientApp/src/app/main/students-list/students-list.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Inject } from "@angular/core";
 import { PerfectScrollbarConfigInterface } from "ngx-perfect-scrollbar";
+import { HttpClient } from "@angular/common/http";
+import { MatTableDataSource } from "@angular/material";
+import { trigger, state, transition, style, animate } from "@angular/animations";
 
 @Component({
   selector: "app-students-list",
   templateUrl: "./students-list.component.html",
   styleUrls: ["./students-list.component.css"],
+  animations: [
+    trigger("detailExpand", [
+      state("collapsed", style({ height: "0px", minHeight: "0" })),
+      state("expanded", style({ height: "*" })),
+      transition(
+        "expanded <=> collapsed",
+        animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      )
+    ])
+  ]
 })
 export class StudentsListComponent implements OnInit {
   public config: PerfectScrollbarConfigInterface = {};
 
-  constructor() {}
+  public dataSource: MatTableDataSource<Student>;
+  public displayedColumns = ['name', 'number', 'course'];
+  expandedElement: Student | null;
 
-  ngOnInit() {}
+
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string,
+  ) {
+    http.get<Student[]>(baseUrl + 'api/students').subscribe(
+      result => {
+        this.dataSource = new MatTableDataSource(result);
+        console.log(result);
+      },
+      error => console.error('error')
+    );
+  }
+
+
+  ngOnInit() {
+  }
+
+}
+
+
+interface Student {
+  id: number;
+  number: number;
+  courseId: number;
+  course: Course;
+  userId: string;
+  user: User;
+}
+
+interface Course {
+  id: number;
+  sigla: string;
+  designação: string;
+}
+
+interface User {
+  name: string;
+  email: string;
 }
